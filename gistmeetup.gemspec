dacbb63305cab0d3cd06a9fd64e2200557b3941f
joshuacalloway <joshua.calloway@gmail.com>
2013-10-25 17:18:58 -0500

    added pushing data to parse

--- gistmeetup.gemspec
+++ gistmeetup.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'gistmeetup'
-  s.version     = '0.0.0'
+  s.version     = '0.0.1'
   s.date        = '2013-10-15'
   s.summary     = "gistmeetup"
   s.description = "A gem that uploads to gist and meetup"
@@ -12,5 +12,7 @@ Gem::Specification.new do |s|
   s.executables << 'gistmeetup'
   s.add_dependency "gist", ">= 4.1.1"
   s.add_dependency "rubymeetup", ">= 3.0" 
+  s.add_dependency "parse-ruby-client", "~> 0.2.0"
+  s.add_dependency "json", ">= 1.8.1"
   s.license       = 'MIT'
 end
--- lib/gistmeetup.rb
+++ lib/gistmeetup.rb
@@ -1,5 +1,10 @@
 require 'gist'
 require 'rmeetup'
+require 'parse-ruby-client'
+require 'json'
+
+Parse.init :application_id => "H4kSq1roztLdYcKu6IdixJgbVtzaoxlEfagYiRYA",
+           :api_key        => "LGcTEzAnxzWUTngaXzdjy4FUixwUSeTJuy8sw56d"
 
 module Gistmeetup
   extend self
@@ -30,9 +35,24 @@ module Gistmeetup
     else
       nextEvent = events[0]
       url = ret["html_url"]
-      RMeetup::Client.post(:event_comment, {:event_id => "#{nextEvent.id}", :comment => "#{comment} - #{url}" })
+      response = RMeetup::Client.post(:event_comment, {:event_id => "#{nextEvent.id}", :comment => "#{comment} - #{url}" })
+      saveMetaData(meetup, comment, nextEvent, url, response)
       "Posted to github on '#{url}' and link to it on '#{meetup}' comments"
     end
+  end
+
+  def saveMetaData(meetup, comment, nextEvent, url, response)
+    json = JSON.parse(response.body)
+    
+    code = Parse::Object.new("Code")
+    code["meetup"] = meetup
+    code["comment"] = comment
+    code["nextEvent_id"] = nextEvent.id
+    code["nextEvent_name"] = nextEvent.name
+    code["member_id"] = json["member_id"].to_s
+    code["member_name"] = json["member_name"]
+    code["event_comment_id"] = json["event_comment_id"].to_s
+    result = code.save
   end
 
   def login!(credentials={})
